fe20cd540daeea63206d7dca92adcce6ff5df34e
John Marshall <john@themillhousegroup.com>
2016-04-25 23:20:03 +1000

    Start the styleguide spidering

---
+++ src/main/scala/com/themillhousegroup/witchhunt/StyleguideScraper.scala
@@ -0,0 +1,34 @@
+package com.themillhousegroup.witchhunt
+
+import org.jsoup.nodes.{ Element, Document }
+import scala.concurrent.Future
+import com.themillhousegroup.scoup.{ ScoupImplicits, Scoup }
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.net.URL
+
+/**
+ * Scrapes / Spiders the given base URL (a styleguide), returning at least one Scoup/JSoup
+ * Document, representing the styleguide and any local pages that link off it.
+ */
+class StyleguideScraper extends ScoupImplicits {
+
+  def visit(url: URL): Future[Set[Document]] = {
+    Scoup.parse(url.toString).map { doc =>
+      val links = doc.select("a").filter(isLocal).map(_.attr("href"))
+      links.map(createFullLocalUrl(url)).foldLeft(Set(doc)) {
+        case (acc, link) =>
+          //FIXME: handle recursion etc
+          Set(doc)
+      }
+    }
+  }
+
+  private def isLocal(link: Element): Boolean = {
+    val href = link.attr("href")
+    href.startsWith("/")
+  }
+
+  private def createFullLocalUrl(base: URL)(link: String): String = {
+    (new java.net.URL(base, link)).toString
+  }
+}
