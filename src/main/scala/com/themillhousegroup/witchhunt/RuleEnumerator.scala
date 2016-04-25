f6a997f5f3e700e0405df7d4032df6a71d176df1
John Marshall <john@themillhousegroup.com>
2016-04-25 17:48:39 +1000

    Wrapping PHCSS

--- src/main/scala/com/themillhousegroup/witchhunt/RuleEnumerator.scala
+++ src/main/scala/com/themillhousegroup/witchhunt/RuleEnumerator.scala
@@ -1,5 +1,30 @@
 package com.themillhousegroup.witchhunt
 
-class RuleEnumerator {
+import com.helger.css.reader.CSSReader
+import com.helger.css.ECSSVersion
+import com.helger.css.decl.{ CSSStyleRule, CascadingStyleSheet }
+import scala.collection.JavaConverters._
+import com.helger.css.writer.CSSWriterSettings
 
+class RuleEnumerator(val source: String) {
+
+  val cssVersion = ECSSVersion.LATEST
+
+  val writerSettings = new CSSWriterSettings(cssVersion)
+
+  val stylesheet: CascadingStyleSheet = CSSReader.readFromString(source, cssVersion)
+
+  def styleRules(): Seq[String] = {
+    stylesheet.getAllStyleRules.asScala.map { styleRule: CSSStyleRule =>
+      styleRule.getAsCSSString(writerSettings, 0)
+    }
+  }
+
+  def fontFaceRules(): Seq[String] = {
+    Nil
+  }
+
+  def mediaRules(): Seq[String] = {
+    Nil
+  }
 }
