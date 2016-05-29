37eddede95dca428531e194ae8eb5996c51553dd
John Marshall <john@themillhousegroup.com>
2016-05-29 14:29:47 +1000

    Pass the declarations within a rule to each Check

--- src/test/scala/com/themillhousegroup/witchhunt/checks/ExcessiveSpecificityCheckSpec.scala
+++ src/test/scala/com/themillhousegroup/witchhunt/checks/ExcessiveSpecificityCheckSpec.scala
@@ -20,19 +20,19 @@ class ExcessiveSpecificityCheckSpec extends Specification with Mockito {
     "Be silent for selectors below the threshold" in {
       val esc = buildESC(100)
 
-      esc.checkSelector(re, "html", mockSourceFileLine, Set.empty) must beNone
+      esc.checkSelector(re, "html", mockSourceFileLine, Nil, Set.empty) must beNone
     }
 
     "Not report selectors with specificity equal to the threshold" in {
       val esc = buildESC(10)
 
-      esc.checkSelector(re, ".header", mockSourceFileLine, Set.empty) must beNone
+      esc.checkSelector(re, ".header", mockSourceFileLine, Nil, Set.empty) must beNone
     }
 
     "Report selectors with specificity above the threshold" in {
       val esc = buildESC(10)
 
-      val result = esc.checkSelector(re, "html body div.header", mockSourceFileLine, Set.empty)
+      val result = esc.checkSelector(re, "html body div.header", mockSourceFileLine, Nil, Set.empty)
 
       result must beSome[Violation]
 
